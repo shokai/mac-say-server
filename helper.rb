2e00a7fd05ba9025244d6c5a9072592d41866c4a
Sho Hashimoto <hashimoto@shokai.org>
2011-06-12 11:32:19 +0900

    use haml

--- helper.rb
+++ helper.rb
@@ -1,6 +1,7 @@
 require 'bundler/setup'
 require 'MeCab'
 require 'yaml'
+require 'haml'
 
 begin
   @@conf = YAML::load open(File.dirname(__FILE__)+'/config.yaml').read
@@ -19,3 +20,6 @@ rescue => e
   exit 1
 end
 
+def app_root
+  "#{env['rack.url_scheme']}://#{env['HTTP_HOST']}#{env['SCRIPT_NAME']}"
+end
--- main.rb
+++ main.rb
@@ -1,34 +1,26 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 
-def app_root
-  "#{env['rack.url_scheme']}://#{env['HTTP_HOST']}#{env['SCRIPT_NAME']}"
+
+before do
+  @title = 'Mac Say Server'
 end
 
 get '/' do
-<<EOS
-<h1>say server</h1>
-<form method="post" action="#{app_root}/say" id="say">
-<input type="text" name="message" size="60"> <input type="submit" name="say" value="say">
-</form>
-<h2>API</h2>
-HTTP-POST #{app_root}/say<br />
-post parameter "message"
-EOS
+  haml :index
 end
 
 post '/say' do
   m = params['message']
   m = m.gsub(/[`"'\r\n;]/, '').chomp.strip
-  puts m
-  puts m = @@mecab.parse(m).map{|i|
+  m = @@mecab.parse(m).map{|i|
     i.split(/\t/)[1]
   }.delete_if{|i|
     i.to_s.size < 1
   }.join('').chomp.strip
   if m.size > 0
     puts cmd = "#{@@conf['saykana']} '#{m}'"
-    puts `#{cmd}`
+    `#{cmd}`
   end
   redirect app_root+'/'
 end
